bcf9cca76380b059380a747afd22a0b58d980c55
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-28 16:32:01 -0600

    creando la estructura inicial del servidor

---
+++ Recomendacion_Viaje/Back/server.js
@@ -0,0 +1,10 @@
+const app = require('./init')
+
+const startServer = () => {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
+
+startServer

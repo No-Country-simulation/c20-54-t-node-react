a6f9d916e5a7cdd60162a5fbf8b2b9d13b777f82
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-08-28 17:35:20 -0600

    llamando la funcion de la conexion de la base de datos inicializar la conexion de la base de datos

--- Recomendacion_Viaje/Back/server.js
+++ Recomendacion_Viaje/Back/server.js
@@ -1,10 +1,11 @@
 const app = require('./init')
 const db = require('./src/db/database')
 
+
 const startServer = () => {
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
-    db.then(() => {
+    db().then(() => {
       console.log('Connected to database')
     }).catch((error) => {
       console.log(error)

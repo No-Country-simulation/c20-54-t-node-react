e25bec7b875ce0f2723fb247a1abfcfcea42cbb1
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-08-28 17:29:52 -0600

    llamando la conexion de la base de datos al inicializar el servidor

--- Recomendacion_Viaje/Back/server.js
+++ Recomendacion_Viaje/Back/server.js
@@ -1,8 +1,14 @@
 const app = require('./init')
+const db = require('./src/db/database')
 
 const startServer = () => {
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
+    db.then(() => {
+      console.log('Connected to database')
+    }).catch((error) => {
+      console.log(error)
+    })
     console.log(`Server is running on port ${PORT}`)
   })
 }

bf88c0dbc641b1aa8c353fec12bbfddffb75b519
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-02 14:27:29 -0600

    creando el modelo ZoneMean

---
+++ Recomendacion_Viaje/Back/src/db/models/ZoneMean.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const zoneMeanSchema = new Schema({
+  meanID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Zone'
+  },
+  type: {
+    type: String,
+    enum: ['economy', 'tourist', 'executive', 'vip'],
+    default: 'economy'
+  },
+  seatsTotal: {
+    type: Number,
+    default: 0
+  },
+  seatsOccupied: {
+    type: Number,
+    default: 0
+  },
+  seatsFree: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'delete'],
+    default: 'active'
+  }
+});

2d1c1564bbc86760c46e8993a11e25a860d40ab4
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-02 14:13:59 -0600

    creando el modelo Mean

---
+++ Recomendacion_Viaje/Back/src/db/models/Mean.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const meanSchema = new Schema({
+  transportID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Transport',
+    required: true
+  },
+  type: String,
+  numberMean: String,
+  seatsTotal: Number,
+  seatsOccupied: Number,
+  seatsFree: Number,
+  prices: [Number],
+  to: String,
+  from: String,
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'delete'],
+    default: 'active'
+  }
+});
+
+moduel.exports = mongoose.model('Mean', meanSchema);

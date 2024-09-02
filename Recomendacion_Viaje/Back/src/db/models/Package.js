bae2f202a48038eaa7430038268b9558b2447d74
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-02 14:41:14 -0600

    creando el modelo Package

---
+++ Recomendacion_Viaje/Back/src/db/models/Package.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const packageSchema = new Schema({
+  from: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  roomID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  meanID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Mean',
+    required: true
+  },
+  priceTotal: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'delete'],
+    default: 'active'
+  }
+});
+
+module.exports = mongoose.model('Package', packageSchema);

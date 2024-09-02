774d1a78eeae24c7d4006928432a5872a65035e8
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-02 17:32:52 -0600

    creando el middleware error global

---
+++ Recomendacion_Viaje/Back/src/middlewares/errorGlobal.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const globalError = (error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    res.status(error.statusCode).json({
+      status: error.status,
+      error: error,
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+}

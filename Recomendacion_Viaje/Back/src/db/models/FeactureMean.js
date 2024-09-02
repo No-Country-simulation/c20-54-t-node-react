28d94ca5c5412faf19c3ee841568d81a51e19bb7
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-02 14:34:44 -0600

    creando el modelo FeactureMean

---
+++ Recomendacion_Viaje/Back/src/db/models/FeactureMean.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const feactureMeanSchema = new Schema({
+  meanID: {
+    type: Schema.Types.ObjectId,
+    ref: 'Mean',
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  service: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'delete'],
+    default: 'active'
+  }
+});
+
+module.exports = mongoose.model('FeactureMean', feactureMeanSchema);

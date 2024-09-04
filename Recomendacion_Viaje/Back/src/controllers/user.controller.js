dd25805d8c3184bc58cdcedc2b74304f603e0ee3
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-04 13:32:14 -0600

    creandoe el controlador getUser

--- Recomendacion_Viaje/Back/src/controllers/user.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/user.controller.js
@@ -1,5 +1,9 @@
 // models
 const User = require('../db/models/User')
+
+// AppError
+const AppError = require('../util/AppError')
+
 const tryCatch = require('../util/tryCatch')
 
 exports.getUsers = tryCatch(async (req, res, next) => {
@@ -10,6 +14,24 @@ exports.getUsers = tryCatch(async (req, res, next) => {
   return res.status(200).json({
     message: 'Users found',
     data: users
+  })
+
+})
+
+exports.getUser = tryCatch(async (req, res, next) => {
+
+  const { id } = req.params
+
+  const user = await User.findById(id)
+    .select('-password -createdAt -updatedAt -__v')
+
+  if (!user) {
+    return next(new AppError('User not found', 404))
+  }
+
+  return res.status(200).json({
+    message: 'User found',
+    data: user
   })
 
 })

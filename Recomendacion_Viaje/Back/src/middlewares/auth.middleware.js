69b9d0643b8b79060f516864074104c4f1cc35b4
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-05 22:38:25 -0600

    creando el middleware auth

---
+++ Recomendacion_Viaje/Back/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+// utils
+const tryCatch = require('../util/tryCatch');
+const AppError = require('../util/AppError');
+
+exports.auth = tryCatch(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  } else {
+    return next(new AppError('error auth', 403));
+  }
+
+  const verified = jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new AppError('error auth', 403));
+    } else {
+      return decoded
+    }
+  });
+
+  req.user = verified;
+})

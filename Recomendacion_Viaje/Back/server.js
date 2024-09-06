bf7c16fed81c715e6e029ab5c4a6349627aed898
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-05 21:36:40 -0600

    instalando el paquete jsonwebtoken

--- Recomendacion_Viaje/Back/server.js
+++ Recomendacion_Viaje/Back/server.js
@@ -1,14 +1,13 @@
 const app = require('./init')
 const db = require('./src/db/database')
 
-
 const startServer = () => {
   const PORT = process.env.PORT || 3000
   app.listen(PORT, () => {
     db().then(() => {
       console.log('Connected to database')
     }).catch((error) => {
-      console.log(error)
+      console.log('error en la base de datos: ', error)
     })
     console.log(`Server is running on port ${PORT}`)
   })
--- Recomendacion_Viaje/Back/src/db/database.js
+++ Recomendacion_Viaje/Back/src/db/database.js
@@ -6,7 +6,10 @@ dotenv.config();
 // connect database
 const db = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URL);
+    const connect = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     return connect
   } catch (error) {
     console.log(error);

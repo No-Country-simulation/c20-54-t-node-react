b3f8822d9f5702f68be780566fb718bbcc488293
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-09-05 19:35:37 -0500

    style: add color and font variables

--- Recomendacion_Viaje/Front/tailwind.config.js
+++ Recomendacion_Viaje/Front/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "primary-color": "#84B6F4",
+      "secondary-color": "#ffffff",
+      "action-color":"#52CF6C"
+    },
+    extend: {
+      fontFamily: {
+        title: [ "Lato", "sans-serif"],
+        body: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }

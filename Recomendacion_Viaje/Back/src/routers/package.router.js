cacf404b02d7c5575614091406e940d210aee9d8
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-06 20:13:23 -0600

    agregando el middleware para validar las querys para obtener los paquetes por precio o/y destino y origen

--- Recomendacion_Viaje/Back/src/routers/package.router.js
+++ Recomendacion_Viaje/Back/src/routers/package.router.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+// middlewares -> package
+const {
+  getPackages: getPackagesValidate
+} = require('../middlewares/expressValidator/package.middleware')
+
 // controllers -> package
 const {
   getPackages
 } = require('../controllers/package.controller');
+const validate = require('../middlewares/validateExpress');
 
 // routers
 
-router.get('/', getPackages);
+router.get('/', validate(getPackagesValidate), getPackages);
 
 module.exports = { packageRouter: router }

193c06e8aa67487d6940e62c02732e15a9196300
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-06 19:33:42 -0600

    creando el controlador para obtener los paquetes por precio y por destino

--- Recomendacion_Viaje/Back/src/controllers/package.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/package.controller.js
@@ -23,6 +23,33 @@ exports.getPackageByPrice = tryCatch(async (req, res, next) => {
   })
 })
 
+exports.getPackages = tryCatch(async (req, res, next) => {
+  const { price, from = '', to = '' } = req.query
+
+  const packages = await Package.find({
+    price: {
+      $lte: price
+    },
+    to: {
+      $gte: to
+    },
+    from: {
+      $lte: from
+    }
+  })
+
+  if (packages.length === 0) return res.status(404).json({
+    message: 'No packages found',
+    data: []
+  })
+
+  return res.status(200).json({
+    message: 'Packages found',
+    data: packages
+  })
+
+})
+
 exports.getPackageById = tryCatch(async (req, res, next) => {
   // obtener el paquete por id pero tambien obtenemos los datos de la referencia  meanID y roomID
   const package = await Package.findById(req.params.id).populate('meanID roomID')

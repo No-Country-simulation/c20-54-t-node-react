50e885120262fdf63b09a910c70668c66349f5a6
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-06 19:39:14 -0600

    creando el archivo package.router.js donde creamos el primer router para obtener los packages por medio del presupuesto y/o origin

---
+++ Recomendacion_Viaje/Back/src/routers/package.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+// controllers -> package
+const {
+  getPackages
+} = require('../controllers/package.controller');
+
+// routers
+
+router.get('/', getPackages);

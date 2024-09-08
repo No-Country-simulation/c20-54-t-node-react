708c16269ee800b478c216ae21e1fa7591c6cc9c
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-07 21:07:21 -0600

    creando el controlador para agregar un paquete al carrito de camporas

--- Recomendacion_Viaje/Back/src/controllers/car.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/car.controller.js
@@ -1,6 +1,35 @@
 // models
+import * as next from 'next'
+import * as next from 'next'
 const Car = require('../db/models/Car')
+const Package = require('../db/models/Package')
 
 // utils
 const AppError = require('../util/AppError')
 const tryCatch = require('../util/tryCatch')
+
+exports.addPackageCar = tryCatch(async (req, res, next) => {
+  const packageID = req.body.packageID
+
+  const car = await Car.findOne({ status: 'pending' })
+
+  if (!car) {
+    const newCar = await Car.create({
+      userID: req.user.id,
+      items: [{ packageID, quantity: 1 }],
+      total: 0,
+    })
+
+    return res.status(201).json({ status: 'success', data: newCar })
+  }
+
+  const findPackage = await Package.findOne({ _id: packageID })
+
+  if (!findPackage) return next(new AppError('Package not found', 404))
+
+  await car.updateOne({ $push: { items: { packageID, quantity: 1 } }, total: car.total + findPackage.priceTotal })
+
+  await car.save()
+
+  return res.status(200).json({ status: 'success', data: car })
+})

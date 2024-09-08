a4e0af1c8cc7e67532ec0e5b96423426c81339f9
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-08 14:42:01 -0600

    corregiendo las importaciones de los controladores

--- Recomendacion_Viaje/Back/src/routers/car.router.js
+++ Recomendacion_Viaje/Back/src/routers/car.router.js
@@ -1,7 +1,7 @@
 const app = require('express');
 
 // controllers -> car
-const { getCar, addPackageCar } = require('../controllers/car.controller')
+const { getCar, addPackageCar, removePackageCar, buyCar } = require('../controllers/car.controller')
 
 // middleware
 const { auth } = require('../middlewares/auth.middleware')
@@ -14,8 +14,8 @@ router.get('/car', auth, getCar)
 
 router.post('/car', auth, addPackageCar)
 
-router.delete('/car', auth, deleteCar)
+router.delete('/car', auth, removePackageCar)
 
-router.put('/car/buy', auth, updateCar)
+router.put('/car/buy', auth, buyCar)
 
 module.exports = { carRouter: router }

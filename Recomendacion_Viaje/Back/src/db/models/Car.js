974044b34ecd94389329751748b16593eadfd17a
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-07 20:29:07 -0600

    quitando la letra 't' en el nombre del esquema car y exportamos el modelo Car

--- Recomendacion_Viaje/Back/src/db/models/Car.js
+++ Recomendacion_Viaje/Back/src/db/models/Car.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const cartSchema = new Schema({
+const carSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   items: [{
     product: {
@@ -22,3 +22,5 @@ const cartSchema = new Schema({
     default: 'pending'
   }
 });
+
+module.exports = mongoose.model('Car', carSchema);

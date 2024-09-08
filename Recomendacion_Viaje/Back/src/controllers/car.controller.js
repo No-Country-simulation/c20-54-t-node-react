0e63a72f1008686d7ffdddb996309ba16ae37fe9
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-07 21:23:50 -0600

    creando el controlador para remover un paquete del carrito de campra

--- Recomendacion_Viaje/Back/src/controllers/car.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/car.controller.js
@@ -33,3 +33,31 @@ exports.addPackageCar = tryCatch(async (req, res, next) => {
 
   return res.status(200).json({ status: 'success', data: car })
 })
+
+exports.removePackageCar = tryCatch(async (req, res, next) => {
+  const packageID = req.body.packageID
+
+  const car = await Car.findOne({ status: 'pending' })
+
+  if (!car) return next(new AppError('Car not found', 404))
+
+  const findPackage = await Package.findOne({ _id: packageID })
+
+  if (!findPackage) return next(new AppError('Package not found', 404))
+
+  const packageInCar = car.items.find((item) => item.packageID.toString() === packageID)
+
+  if (!packageInCar) return next(new AppError('Package not found in car', 404))
+
+  if (packageInCar.quantity > 1) {
+    await car.updateOne({ $set: { 'items.$[elem].quantity': packageInCar.quantity - 1 } }, { arrayFilters: [{ 'elem.packageID': packageID }] })
+  } else {
+    await car.updateOne({ $pull: { items: { packageID } } })
+  }
+
+  await car.updateOne({ $pull: { items: { packageID } }, total: car.total - findPackage.priceTotal })
+
+  await car.save()
+
+  return res.status(200).json({ status: 'success', data: car })
+})

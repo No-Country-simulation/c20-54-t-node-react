3122137f895b0943c2540bbb1190e875a914a6f9
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-09 16:06:05 -0600

    creando el campo category en el modelo package

--- Recomendacion_Viaje/Back/src/db/models/Package.js
+++ Recomendacion_Viaje/Back/src/db/models/Package.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const packageSchema = new Schema({
+  cateogry: {
+    type: String,
+    enum: ['completed', 'hotel', 'restaurant'],
+    required: true
+  },
   from: {
     type: String,
     required: true

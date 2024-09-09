257edc393b91ce5adc7842892f4a2b15b656bbf5
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-09 16:32:31 -0600

    cambiando la estructura de los campos del modelo, en algunos si tipo de datos

--- Recomendacion_Viaje/Back/src/db/models/Mean.js
+++ Recomendacion_Viaje/Back/src/db/models/Mean.js
@@ -7,17 +7,52 @@ const meanSchema = new Schema({
     ref: 'Transport',
     required: true
   },
-  type: String,
-  numberMean: String,
-  seatsTotal: Number,
-  seatsOccupied: Number,
-  seatsFree: Number,
-  prices: [Number],
-  to: String,
-  from: String,
-  description: Object,
-  images: [String],
-  firstImage: String,
+  type: {
+    type: String,
+    required: true
+  },
+  numberMean: {
+    type: Number,
+    required: true
+  },
+  seatsTotal: {
+    type: Number,
+    required: true
+  },
+  seatsOccupied: {
+    type: Number,
+    required: true
+  },
+  seatsFree: {
+    type: Number,
+    required: true
+  },
+  prices: {
+    type: [Number],
+    required: true
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  from: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: Object,
+    default: {
+      content: String
+    }
+  },
+  images: {
+    type: [String],
+    default: []
+  },
+  firstImage: {
+    type: String,
+    required: true
+  },
   status: {
     type: String,
     enum: ['active', 'inactive', 'delete'],

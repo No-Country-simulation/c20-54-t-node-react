ec8c2da1b04e19a21cd24e9338eb191d0f611ce0
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-08 20:31:20 -0600

    creando el middleware para validar los datos para el endpoint registro usuario

--- Recomendacion_Viaje/Back/src/middlewares/expressValidator/user.middleware.js
+++ Recomendacion_Viaje/Back/src/middlewares/expressValidator/user.middleware.js
@@ -17,3 +17,40 @@ exports.loginBody = [
     .withMessage('Password must be a string')
 
 ]
+
+exports.registerUser = [
+  body('username')
+    .notEmpty()
+    .withMessage('Username is required')
+    .isLength({ min: 5 })
+    .withMessage('Username must be at least 5 characters long')
+    .isString()
+    .withMessage('Username must be a string'),
+  body('lastName')
+    .notEmpty()
+    .withMessage('Last Name is required')
+    .isString()
+    .withMessage('Last Name must be a string'),
+  body('dateBirth')
+    .notEmpty()
+    .withMessage('Date of Birth is required')
+    .isISO8601()
+    .withMessage('Date of Birth must be a valid date'),
+  body('idAt')
+    .notEmpty()
+    .withMessage('Gender is required')
+    .isString()
+    .withMessage('Gender must be a string'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email must be a valid email address'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .isString()
+    .withMessage('Password must be a string')
+]

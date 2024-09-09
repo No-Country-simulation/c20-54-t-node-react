77866c991abc427c3d6fcd19999cb2cf38fad624
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-08 20:03:54 -0600

    creando el controlador login usuario

--- Recomendacion_Viaje/Back/src/controllers/user.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/user.controller.js
@@ -1,22 +1,13 @@
 // models
 const User = require('../db/models/User')
 
+// jsonwebtoken
+const jwt = require('jsonwebtoken')
+
 // AppError
 const AppError = require('../util/AppError')
 
 const tryCatch = require('../util/tryCatch')
-
-exports.getUsers = tryCatch(async (req, res, next) => {
-
-  const users = await User.find()
-    .select('-password -createdAt -updatedAt -__v')
-
-  return res.status(200).json({
-    message: 'Users found',
-    data: users
-  })
-
-})
 
 exports.getUser = tryCatch(async (req, res, next) => {
 
@@ -32,6 +23,36 @@ exports.getUser = tryCatch(async (req, res, next) => {
   return res.status(200).json({
     message: 'User found',
     data: user
+  })
+
+})
+
+exports.loginUser = tryCatch(async (req, res, next) => {
+
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    return next(new AppError('User not found', 404))
+  }
+
+  const isMatch = await user.comparePassword(password)
+
+  if (!isMatch) {
+    return next(new AppError('Invalid credentials', 401))
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+  user.password = undefined
+
+  return res.status(200).json({
+    message: 'User logged in',
+    data: {
+      user,
+      token
+    }
   })
 
 })

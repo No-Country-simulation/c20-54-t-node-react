9483f49284709a7dac9f78734c6170af4aed4d9e
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-09 20:57:47 -0600

    cambiando la estructura del campo description

--- Recomendacion_Viaje/Back/src/db/models/Transport.js
+++ Recomendacion_Viaje/Back/src/db/models/Transport.js
@@ -6,8 +6,13 @@ const transportSchema = new Schema({
   rating: Number,
   images: [String],
   firstImage: String,
-  description: Object,
-  service: [String],
+  description: { // es de tipo objeto para describir la campania de transporte
+    type: Object,
+    default: {
+      content: String,
+    }
+  },
+  service: [String], // es de tipo array que guarda string para ver los servicios que da la compania como internet, comida, etc...
   status: {
     type: String,
     default: 'active'

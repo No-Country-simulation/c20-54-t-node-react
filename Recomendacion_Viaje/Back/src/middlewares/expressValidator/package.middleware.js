68192ba2f0b85b93208741fde11443f510dfef86
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-09 21:30:35 -0600

    creando el middleware para validar los querys pasados por query

--- Recomendacion_Viaje/Back/src/middlewares/expressValidator/package.middleware.js
+++ Recomendacion_Viaje/Back/src/middlewares/expressValidator/package.middleware.js
@@ -3,5 +3,8 @@ const { body, query } = require('express-validator');
 exports.getPackages = [
   query('price').optional().isNumeric().withMessage('price must be a number value'),
   query('from').optional().isString().withMessage('form must be a string value'),
-  query('to').optional().isString().withMessage('to must be a string value')
+  query('to').optional().isString().withMessage('to must be a string value'),
+  query('category').optional().isString().withMessage('category must be a string value'),
+  query('page').optional().isNumeric().withMessage('page must be a number value'),
+  query('limit').optional().isNumeric().withMessage('limit must be a number value')
 ]

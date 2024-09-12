fdcf988c6873e05cdcc47aa22bd875e9d88c7278
Adela_olivera <adela090700@gmail.com>
2024-09-11 21:54:20 -0300

    nueva ruta para el Booking

--- Recomendacion_Viaje/Front/src/router/AppRouter.jsx
+++ Recomendacion_Viaje/Front/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from '../components/Layout/Layout';
 import Landing from '../pages/Landing';
 import LoginBooking from '../pages/LoginBooking';
+import Booking from '../pages/Booking';
 import DetailsCard from '../pages/DetailsCard';
 import Home from '../pages/Home';
 
@@ -12,7 +13,8 @@ const AppRouter = () => {
         <Route element={<Layout/>}>
             <Route path='/begins' element={<Landing/>} />
             <Route index path='/' element={<Home/>} />
-            <Route path='/booking' element={<LoginBooking/>} />
+            <Route path='/login' element={<LoginBooking/>} />
+            <Route path='/booking' element={<Booking/>} />
             <Route path='/details/:idCard' element={<DetailsCard/>} />
         </Route>
 

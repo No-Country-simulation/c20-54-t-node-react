942556d6f7b1c25c015ea356a77bec7adfd16e01
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-12 11:52:37 -0600

    arreglando el router para obtener los paquetes y para crear los comentarios en los paquetes

--- Recomendacion_Viaje/Back/src/routers/package.router.js
+++ Recomendacion_Viaje/Back/src/routers/package.router.js
@@ -4,26 +4,26 @@ const router = express.Router();
 
 // middlewares -> package
 const {
-  getRandomPackages: getPackagesValidate
+  getPackages: getPackageBody,
+  packageComents
 } = require('../middlewares/expressValidator/package.middleware')
 const { auth } = require('../middlewares/auth.middleware');
 
 
 // controllers -> package
 const {
-  getRandomPackages,
+  getPackages,
   getPackageById,
   //getPackagesAll,
   createCommentPackage
 } = require('../controllers/package.controller');
 const validate = require('../middlewares/validateExpress');
-const { createCommentPackage: createCommentPackagebody } = require('../middlewares/expressValidator/package.middleware');
 
 // routers
 
-router.get('/', validate(getPackagesValidate), getRandomPackages);
+router.get('/', validate(getPackageBody), getPackages);
 router.get('/:id', getPackageById);
 //router.get('/all', getPackagesAll);
-router.post('/:id/comment', auth, validate(createCommentPackagebody), createCommentPackage);
+router.post('/:id/comment', auth, validate(packageComents), createCommentPackage);
 
 module.exports = { packageRouter: router }

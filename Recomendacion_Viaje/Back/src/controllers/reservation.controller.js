05623564fedca84016aa8878e1f15895e086f1be
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-13 13:16:04 -0600

    creando el controlador para crear una servacion

--- Recomendacion_Viaje/Back/src/controllers/reservation.controller.js
+++ Recomendacion_Viaje/Back/src/controllers/reservation.controller.js
@@ -1,5 +1,7 @@
 // models 
 const Reservation = require('../db/models/Reservation')
+const Car = require('../db/models/Car')
+const Package = require('../db/models/Package')
 
 // utils
 const AppError = require('../utils/AppError')
@@ -14,5 +16,31 @@ exports.getReservations = tryCatch(async (req, res, next) => {
     .skip((page - 1) * limit)
 
   res.status(200).json({ status: 'success', data: reservations })
+})
+
+exports.createReservation = tryCatch(async (req, res, next) => {
+  const { packageID, quantity = 1, isAuthor = true } = req.body
+
+  if (packageID) {
+    const package = await Package.findOne({ _id: packageID, status: 'active' })
+
+    if (!package) return next(new AppError('Package not found', 404))
+
+    const priceTotal = package.priceTotal * quantity
+    const newCar = await Car.create({
+      userID: req.user.id,
+      items: [{ packageID, quantity }],
+      priceTotal,
+      packageTotal: quantity,
+    })
+
+    const reservations = new Reservation({
+      userID: req.user.id,
+      carID: newCar._id,
+    })
+  }
+
+  const car = await Car.findOne({ userID: req.user.id })
+
 })
 

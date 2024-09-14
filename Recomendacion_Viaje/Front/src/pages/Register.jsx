50147ab610129bfbc936ad4c3b9bcc448fd40dfb
Adela_olivera <adela090700@gmail.com>
2024-09-14 15:22:13 -0300

    Update Register.jsx
    se actualizan los datos y tipo de datos para poder conectar al back

--- Recomendacion_Viaje/Front/src/pages/Register.jsx
+++ Recomendacion_Viaje/Front/src/pages/Register.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import "../assets/css/LoginBanner.css";
 
-const RegisterBooking = () => {
+const LoginBooking = () => {
   const [formData, setFormData] = useState({
-    email: "",
+    email: "", // Cambié 'username' por 'email' para mayor consistencia
     password: "",
   });
 
@@ -27,91 +27,95 @@ const RegisterBooking = () => {
     setSuccess(false);
 
     try {
-      // Hacemos la solicitud POST para registrar
-      const response = await axios.post("", formData);
+      console.log("formData:", formData);
+      // Hacemos la solicitud POST para autenticar
+      const response = await axios.post(
+        "https://c20-54-t-node-react.onrender.com/api/v1/users/",
+        formData
+      );
 
       console.log("Respuesta del servidor:", response.data);
 
-      if (response.data.success) {
-        // Suponemos que el servidor devuelve un éxito al registrar
-        setSuccess(true);
+      // Suponemos que el servidor devuelve un token si la autenticación es exitosa
+      if (response.data.token) {
+        // Almacenar el token en el localStorage (o cookies) y manejar el éxito
+        localStorage.setItem("token", response.data.token);
+        setSuccess(true); // Cambiamos el estado a éxito
       }
     } catch (err) {
-      console.error("Error en el registro:", err);
-      setError("Error al registrar. Inténtalo nuevamente.");
+      console.error("Error de autenticación:", err);
+      setError("Email o contraseña incorrectos"); // Muestra un mensaje de error
     } finally {
-      setLoading(false);
+      setLoading(false); // Deshabilita el estado de carga
     }
   };
 
   return (
-    <div className="w-full">
-      <div className="login-container">
-        <div className="login-banner">
-          <h1 className="font-bold font-title text-primary-color">
-            Crear una cuenta
-          </h1>
-          <p className="text-primary-color font-bold">
-            Regístrate para empezar
-          </p>
-        </div>
-        <div className="login-form bg-bg-info font-bold w-full">
-          <form onSubmit={handleSubmit}>
-            <h2>Registro</h2>
+    <>
+      <div className="w-full">
+        <div className="login-container">
+          <div className="login-banner">
+            <h1 className="font-bold font-title text-primary-color">
+              Bienvenido
+            </h1>
+            <p className=" text-primary-color font-bold">
+              Inicia sesión para continuar
+            </p>
+          </div>
+          <div className="login-form bg-bg-info font-bold  w-full">
+            <form onSubmit={handleSubmit}>
+              <h2>Login</h2>
 
-            {error && <p className="text-red-500">{error}</p>}
-            {success && (
-              <p className="text-green-500">
-                ¡Registro exitoso! Ahora puedes iniciar sesión.
-              </p>
-            )}
+              {error && <p className="text-red-500">{error}</p>}
+              {success && (
+                <p className="text-green-500">¡Inicio de sesión exitoso!</p>
+              )}
 
-            <div className="my-4 flex flex-col justify-center items-center font-bold">
-              <label htmlFor="email">email:</label>
-              <input
-                className="rounded-full flex text-sm py-2 px-4 md:me-0"
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div className=" my-4 flex flex-col justify-center items-center font-bold">
+                <label htmlFor="email">Email:</label>
+                <input
+                  className="rounded-full flex text-sm  py-2 px-4 md:me-0 "
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-            <div className="my-4 flex flex-col justify-center items-center font-bold">
-              <label htmlFor="password">Contraseña:</label>
-              <input
-                className="rounded-full flex text-sm py-2 px-4"
-                type="password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="my-4 flex flex-col justify-center items-center">
-              <button
-                className="flex text-sm bg-primary-color text-secondary-color font-bold py-2 px-4 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
-                type="submit"
-                disabled={loading}
-              >
-                {loading ? "Registrando..." : "Registrarse"}
-              </button>
-            </div>
-
-            <div className="registrarse">
-              <button className="registrarse">
-                <Link to={"/login"}>¿Ya tienes una cuenta? Inicia sesión</Link>
-              </button>
-            </div>
-          </form>
+              <div className=" my-4 flex flex-col justify-center items-center font-bold">
+                <label htmlFor="password">Contraseña:</label>
+                <input
+                  className="rounded-full flex text-sm  py-2 px-4 "
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className=" my-4 flex flex-col justify-center items-center">
+                <button
+                  className="flex text-sm bg-primary-color text-secondary-color font-bold py-2 px-4 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
+                  type="submit"
+                  disabled={loading}
+                >
+                  {loading ? "Iniciando sesión..." : "Iniciar Sesión"}
+                </button>
+              </div>
+              <div className="registrarse ">
+                <button className=" registrarse">
+                  <Link to={"/register "}>Registrarse</Link>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default RegisterBooking;
+export default LoginBooking;

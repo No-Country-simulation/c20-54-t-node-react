b2628fe5259d0032da1679efa394133a1cef43e9
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-15 14:46:21 -0600

    exportando el middleware existyUserAuth

--- Recomendacion_Viaje/Back/src/middlewares/existyUser.middleware.js
+++ Recomendacion_Viaje/Back/src/middlewares/existyUser.middleware.js
@@ -1,7 +1,7 @@
 const User = require('../db/models/User')
 const tryCatch = require('../util/tryCatch')
 
-const existyUserAuth = tryCatch(async (req, res, next) => {
+exports.existyUserAuth = tryCatch(async (req, res, next) => {
   const id = req.user.id
 
   const User = await User.findById(id)

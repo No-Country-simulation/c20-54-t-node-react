0d1c471426387503fe1b00fb81c887ace4ed830d
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-16 23:44:27 -0600

    agregando el middleware para verificar si existe el usuario autentificado

--- Recomendacion_Viaje/Back/src/routers/reservation.router.js
+++ Recomendacion_Viaje/Back/src/routers/reservation.router.js
@@ -5,9 +5,10 @@ const { createReservation } = require('../controllers/reservation.controller')
 
 // middleware
 const { auth } = require('../middlewares/auth.middleware')
+const { existyUserAuth } = require('../middlewares/existyUser.middleware')
 
 const router = express.Router()
 
 // router
 
-router.post('/', auth, createReservation)
+router.post('/', auth, existyUserAuth, createReservation)

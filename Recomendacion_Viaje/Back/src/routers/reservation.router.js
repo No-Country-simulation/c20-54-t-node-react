db4c65067965dcffe66335c49593b4c6e6e6434f
Luis Ariel Retana Hernandez <developerluisarielrh@gmail.com>
2024-09-19 15:05:40 -0600

    creando el router para obtener una reservacion por id con su respectivo controlador

--- Recomendacion_Viaje/Back/src/routers/reservation.router.js
+++ Recomendacion_Viaje/Back/src/routers/reservation.router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 // controller
-const { createReservation, getReservations } = require('../controllers/reservation.controller')
+const { createReservation, getReservations, getReservationById } = require('../controllers/reservation.controller')
 
 // middleware
 const { auth } = require('../middlewares/auth.middleware')
@@ -12,7 +12,7 @@ const router = express.Router()
 // router
 
 router.get('/', auth, existyUserAuth, getReservations)
-router.get('/:id', auth, existyUserAuth, getReservations)
+router.get('/:id', auth, existyUserAuth, getReservationById)
 router.post('/', auth, existyUserAuth, createReservation)
 
 module.exports = { reservationRouter: router }

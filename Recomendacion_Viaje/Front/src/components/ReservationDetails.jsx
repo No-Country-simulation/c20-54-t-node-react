0158dafc1a9161856dacad1a4518345e00ea0ef0
Adela_olivera <adela090700@gmail.com>
2024-09-20 13:44:35 -0300

    Update ReservationDetails.jsx
    conexión con el modal y el back

--- Recomendacion_Viaje/Front/src/components/ReservationDetails.jsx
+++ Recomendacion_Viaje/Front/src/components/ReservationDetails.jsx
@@ -1,39 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
+import { getReservationById } from "../services/ReservationsServices";
 const ReservationDetails = () => {
-  const { idReservation } = useParams(); // Obtener el ID de la reserva de la URL
   const [reservationData, setReservationData] = useState(null);
+  const { idReservation } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    // Llamada a la API para obtener los detalles de la reserva;
-    const fetchReservationDetails = async () => {
-      // console.log("reserva id", idReservation);
-      try {
-        const response = await axios.get(
-          `https://c20-54-t-node-react.onrender.com/api/v1/reservation/${idReservation}`
-        );
-        setReservationData(response.data); // Guardamos los datos obtenidos
-      } catch (err) {
-        setError("Error al cargar los detalles de la reserva");
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (idReservation != null) {
+      // Llamada a la API para obtener los detalles de la reserva;
+      const fetchReservationDetails = async () => {
+        const token = localStorage.getItem("token");
+        const data = {
+          token: token,
+          id: idReservation,
+        };
 
-    fetchReservationDetails();
+        getReservationById(data)
+          .then((response) => {
+            console.log("response", response.data);
+            setReservationData(response);
+          })
+          .catch((e) => console.error(e));
+
+        // try {
+        //   const response = await axios.get(
+        //     `https://c20-54-t-node-react.onrender.com/api/v1/reservation/(id)`
+        //   );
+        //   setReservationData(response.data.id); // Guardamos los datos obtenidos
+        // } catch (err) {
+        //   setError("Error al cargar los detalles de la reserva");
+        // } finally {
+        //   setLoading(false);
+        // }
+      };
+
+      fetchReservationDetails();
+    }
   }, [idReservation]); // Cargar los datos cuando cambia el ID de la reserva
 
-  if (loading) {
-    return <p>Cargando...</p>;
-  }
+  // if (loading) {
+  //   return <p>Cargando...</p>;
+  // }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+  // if (error) {
+  //   return <p>{error}</p>;
+  // }
 
   return (
     <div className="modal-overlay">
@@ -47,14 +61,9 @@ const ReservationDetails = () => {
             <p>
               <strong>Apellido:</strong> {reservationData.lastName}
             </p>
-            <p>
-              <strong>DNI:</strong> {reservationData.idAt}
-            </p>
+
             <p>
               <strong>Email:</strong> {reservationData.email}
-            </p>
-            <p>
-              <strong>Fecha de Nacimiento:</strong> {reservationData.dateBirth}
             </p>
 
             {/* Otros detalles de la reserva */}
@@ -68,3 +77,4 @@ const ReservationDetails = () => {
 };
 
 export default ReservationDetails;
+

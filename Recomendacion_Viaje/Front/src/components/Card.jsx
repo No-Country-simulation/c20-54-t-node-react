0da1fcf9f9717282002d7419cd20d69413e4299a
Alain Sosa <alain.sosa.v@gmail.com>
2024-09-20 12:47:49 -0400

    Change data and add hours

--- Recomendacion_Viaje/Front/src/components/Card.jsx
+++ Recomendacion_Viaje/Front/src/components/Card.jsx
@@ -1,45 +1,39 @@
 import Stars from "./Stars";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import banner from "../assets/spinner.gif";
+import Loader from "./Loader/Loader";
 
+const Card = ({ data = {}, loader = false }) => {
+  console.log(banner);
+  return (
+    <div className="w-2/3 h-max rounded overflow-hidden shadow-lg mt-10 -ml-20  ">
+      <h1 className="text-3xl font-extrabold dark:text-black mb-4">
+        {data?.package?.title}{" "}
+      </h1>
+      <div className="pb-3">
+        <Stars rating={data?.package?.rating} />
+      </div>
+      {loader ? (
+        <div className="flex justify-center items-center h-96 bg-gray-800">
+          <Loader />
+        </div>
+      ) : (
+        <figure className=" mb-2 w-full h-96 object-cover relative mt-6">
+          <img
+            className="transition-transform duration-300 transform hover:scale-110 w-full h-full object-cover"
+            src={data?.package?.firstImage}
+            alt="montanias"
+          />
+        </figure>
+      )}
 
-const Card = ({data={}}) =>{
-console.log(data)
-    return(
-        
-            <div className="w-2/3 h-max rounded overflow-hidden shadow-lg mt-10 -ml-20  ">
-                <h1 className="text-3xl font-extrabold dark:text-black mb-4">{data?.package?.title} </h1>
-                <div className="pb-3">
-                 <Stars rating={data?.package?.rating}/>
-                </div>
-                <figure className=" mb-2 w-full h-96 object-cover relative mt-6"  >
-                    <img className="transition-transform duration-300 transform hover:scale-110 w-full h-full object-cover"src={data?.package?.firstImage} alt="montanias" />
-                    
-                </figure>
-                <div className="flex">
-                   
-                   {
-                    data?.package?.image.map(data=>{
-                        return(  
-                        <figure className="w-50 ml-0 mb-1 mr-1" >
-                            <img src={data} alt="" />
-                        </figure>)
-                    })
-                   }
+      <article className="w-full h-26 rounded overflow-hidden shadow-lg mt-6 text-sm p-2 ">
+        <div>
+          <p>{data?.package?.description?.content}</p>
+        </div>
+      </article>
+    </div>
+  );
+};
 
-                </div>
-                <article className="w-full h-26 rounded overflow-hidden shadow-lg mt-6 text-sm p-2 ">
-                   <div>
-                        <p>
-                            {data?.package?.description?.content}
-                        </p>
-                   </div>
-                  
-                  
-                </article>
-                
-            </div>
-
-    )
-}
-
-export default Card
+export default Card;
--- Recomendacion_Viaje/Front/src/pages/DetailsCard.jsx
+++ Recomendacion_Viaje/Front/src/pages/DetailsCard.jsx
@@ -1,30 +1,27 @@
-import Card from "../components/Card"
-import Form from "../components/Form"
-import Coments from "../components/Coments"
-import React, { useEffect, useState } from 'react';
+import Card from "../components/Card";
+import Form from "../components/Form";
+import Coments from "../components/Coments";
+import { useEffect } from "react";
 import useGetPackage from "../services/useGetPackage";
 import { useParams } from "react-router-dom";
 
 function DetailsCard() {
-  const { idCard } = useParams(); 
-const {fetchData,data}=useGetPackage(idCard);
+  const { idCard } = useParams();
+  const { fetchData, data, loader } = useGetPackage(idCard);
   useEffect(() => {
     fetchData();
     window.scrollTo(0, 0);
   }, []);
-  
-    return (
-      <>
-        
-          <div className="flex ml-80 flex-col md:flex-row md:space-x-4" >
-            <Card data={data}/>
-            <Form/>
-          </div>
-          <Coments packageId={data}/>
-        
-      </>
-    )
-  }
-  
-  export default DetailsCard
-  
+
+  return (
+    <>
+      <div className="flex ml-80 flex-col md:flex-row md:space-x-4">
+        <Card data={data} loader={loader} />
+        <Form data={data} />
+      </div>
+      <Coments packageId={data} />
+    </>
+  );
+}
+
+export default DetailsCard;
